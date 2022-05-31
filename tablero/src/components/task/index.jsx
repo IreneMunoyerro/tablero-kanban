1916847749ffb5c9df54bb833b7120bd2b1855fa
lauralagares <lauralagares1992@example.com>
2022-05-31 23:54:35 +0200

    ULTIMO UPDATE DE LAURA POR HOY SUERTE CHICASSS!!!

--- tablero/src/components/task/index.jsx
+++ tablero/src/components/task/index.jsx
@@ -4,25 +4,31 @@ import { BsTrash } from 'react-icons/bs';
 import { AiOutlineCheckCircle } from 'react-icons/ai';
 
 function Task({task, onDeleteTask}) {
-
-    
+  
     const handleRemove = () => {
         onDeleteTask(task.id) // pasamos al padre al accion
     }
 
-    
     return (
         <>
             <div className='task__container'>
+
                 <section className='task-title'>
+
                     <div className={task.status==="done"? "status-cancel":"status-check"}>
                         <AiOutlineCheckCircle></AiOutlineCheckCircle>
+
                     </div>
+
                     <p className='task-text'>{task.title}</p>
-                    <p>{task.date}#{task.id}</p>
+
+               
                     <span onClick={handleRemove}><BsTrash></BsTrash></span>
+
                 </section>
+
                 <section className='task-created'>
+                <p>#{task.id} created on {task.date}</p>
                 </section>
             </div>
         </>
